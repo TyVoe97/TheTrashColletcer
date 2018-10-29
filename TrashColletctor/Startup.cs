f851305b3d35e3b5e91b749474027c7fe35b7666
TyVoe97 <tydud2016@gmail.com>
2018-10-29 15:38:45 -0500

    added roles into the program

--- TrashColletctor/Startup.cs
+++ TrashColletctor/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TrashColletctor.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrashColletctor.Startup))]
 namespace TrashColletctor
@@ -9,6 +12,33 @@ namespace TrashColletctor
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
         }
     }
+
+    private void createRoles()
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+        if(!roleManager.RoleExists("Customer"))
+        {
+            var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            role.Name = "Customer";
+            roleManager.Create(role);
+        }
+        if(!roleManager.RoleExists("Employee"))
+        {
+            var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            role.Name = "Employee";
+            roleManager.Create(role);
+        }
+
+
+        //[assembly: OwinStartUpAttribute(typeof(Creating_And_Assigning_Roles))]
+        //namespace Creating_And_Assigning_Roles
+    
+
+    }
+   
+    
 }
