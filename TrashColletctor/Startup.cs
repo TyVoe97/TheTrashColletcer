3a3ba83572e2b4924bdb56877d92633cf83140d7
TyVoe97 <tydud2016@gmail.com>
2018-10-29 09:13:27 -0500

    first inital commit and I added a customer class and a empplyee class

---
+++ TrashColletctor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashColletctor.Startup))]
+namespace TrashColletctor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
