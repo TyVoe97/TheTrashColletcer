e4cf6a1cd555ca4aebde0797d9a87a569ab90958
TyVoe97 <tydud2016@gmail.com>
2018-11-01 13:45:44 -0500

    updated the customer and Employee model

--- TrashColletctor/Models/Customer.cs
+++ TrashColletctor/Models/Customer.cs
@@ -9,23 +9,39 @@ namespace TrashColletctor.Models
     public class Customer
     {
         [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "Zipcode")]
         public int Zipcode { get; set; }
+        [Display(Name = "Phone Number")]
         public int PhoneNumber { get; set; }
+        [Display(Name = "Email")]
         public string email { get; set; }
+        [Display(Name = "Password")]
         public string password { get; set; }
+        [Display(Name = "Address")]
         public string Address { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+        
 
     }
     public class Employee
     {
         [Key]
+        public int Id { get; set; }
+        [Display(Name = " First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "Employee Id")]
         public int emplyeeId { get; set; }
+        [Display(Name = "Email")]
         public string email { get; set; }
+        [Display(Name = "Password")]
         public string password { get; set; }
 
     }
